465a8fd16a23a74ad4b7f2d10eaa5dea00272ef1
Moazam5 <m.moazam29@gmail.com>
2021-01-21 01:53:40 -0500

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Moazam Mir on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
